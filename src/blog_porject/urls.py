167272c909ce7bf70e84e38224f4253805d1117c
batavinash <git.batavinash@gmail.com>
2019-12-28 23:55:48 +0530

    init

---
+++ src/blog_porject/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("blog.urls")),
+    path("api-auth", include("rest_framework.urls")),
+    path("api/rest-auth/", include("rest_auth.urls")),
+]
